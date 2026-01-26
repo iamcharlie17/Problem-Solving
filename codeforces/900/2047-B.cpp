e957e22978c6c3824ce4caed24224617f34b9b31
iamcharlie17 <riyadhosen40@gmail.com>
2026-01-26 20:24:32 +0600

    cf 2047 B

---
+++ codeforces/900/2047-B.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int main() {
+    int t;
+    cin >> t;
+
+    while (t--) {
+        int n;
+        cin >> n;
+        string s;
+        cin >> s;     
+
+        vector<int> freq(26, 0);
+
+        for(char c : s) freq[c - 'a']++;
+
+        char maxChar = 'a';
+
+        for(int i = 0; i<26; i++) {
+            if(freq[i] > freq[maxChar - 'a']) {
+                maxChar = 'a' + i;
+            }
+        }
+
+        for(int i = 0; i<n; i++) {
+            if(s[i] != maxChar) {
+                s[i] = maxChar;
+                break;
+            }
+        }
+
+        cout << s << endl;
+
+    }
+    return 0;
+}
